76e75995232ea46c4831e6458c935a3877ba47b1
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-05-07 17:12:30 -0700

    Added React Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 
-const testing = (
-  <div>
-    <h1>testing</h1>
-    <p>testing testing testing shshfsdj</p>
-  </div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  testing
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
