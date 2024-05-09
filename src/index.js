d3e7592230b5732dff66c2fde62cf98812eb099e
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-05-08 22:21:27 -0700

    Added firebase config stuff to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './components/App';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXARIC1CxeowtoBkz4LgVGI0TDakFuDbw",
+  authDomain: "husky-uxplorers.firebaseapp.com",
+  projectId: "husky-uxplorers",
+  storageBucket: "husky-uxplorers.appspot.com",
+  messagingSenderId: "9456045489",
+  appId: "1:9456045489:web:fd6b1e1f96da743cc88190"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
