b260c04662f4b58ade8f564156dbb49947237e3e
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-05-15 19:01:48 -0700

    connected portfolio json to gallery page

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,10 +2,13 @@ import React from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
 
-let DATA;
+let PROJECT_DATA;
+let PORTFOLIO_DATA;
 
 export default function Gallery (props) {
-  DATA = props.data;
+  PROJECT_DATA = props.projectData;
+  PORTFOLIO_DATA = props.portfolioData;
+
   return (
     <div>
       <NavBar />
@@ -36,7 +39,7 @@ function GalleryContent(props) {
   return (
     <div>
       <GalleryProjects />
-      <GalleryPortfolios />
+      {/* <GalleryPortfolios /> */}
     </div>
   );
 }
@@ -45,17 +48,28 @@ function GalleryProjects(props) {
   return (
     <section className="projects container">
       <h2 className="mt-5">Projects</h2>
-      <ProjectsRow />
+      <ProjectCardRow data={PROJECT_DATA} />
     </section>
   );
 }
 
-function ProjectsRow(props) {
-  const row = DATA.map((projectData) => {
-    let {metadata, authorData} = projectData;
+// function GalleryPortfolios(props) {
+//   console.log(PORTFOLIO_DATA);
+//   return (
+//     <section className="projects container">
+//       <h2 className="mt-5">Portfolios</h2>
+//       <PortfolioCardRow data={PORTFOLIO_DATA} />
+//     </section>
+//   )
+// }
+
+function ProjectCardRow(props) {
+  let data = props.data;
+  const row = data.map((currData) => {
+    let {metadata, authorData} = currData;
     return (
       <ProjectCard
-        data={projectData}
+        data={currData}
         key={authorData.author + metadata.title}
       />
     );
@@ -88,97 +102,58 @@ function ProjectCard(props) {
   );
 }
 
-function GalleryPortfolios(props) {
-  return (
-    <section className="portfolios container">
-      <h2 className="mt-5">Portfolios</h2>
-      <div className="row row-cols-1 row-cols-md-4">
-        <div className="col">
-          <div className="card block">
-            <a href="portfolio_example.html">
-              <img src="img/portfolios/port1.jpeg" className="card-img-top" alt="Maya Patel's Portfolio" />
-            </a>
-            <div className="card-body">
-              <h3 className="card-title">Maya Patel </h3>
-              <img className="arrow" src="img/icons/arrow.png" />
-              <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
-              <p className="card-text">HCDE '26</p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card block">
-            <img src="img/portfolios/port2.jpeg" className="card-img-top" alt="Noah Martinez's Portfolio" />
-            <div className="card-body">
-              <h3 className="card-title">Noah Martinez</h3>
-              <img className="arrow" src="img/icons/arrow.png" />
-              <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
-              <p className="card-text">Info '25</p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card block">
-            <img src="img/portfolios/port3.jpeg" className="card-img-top" alt="Alec Santiago's Portfolio" />
-            <div className="card-body">
-              <h3 className="card-title">Alec Santiago</h3>
-              <img className="arrow" src="img/icons/arrow.png" />
-              <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
-              <p className="card-text">Info '24</p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card block">
-            <img src="img/portfolios/port4.jpeg" className="card-img-top" alt="Ava Kim's Portfolio" />
-            <div className="card-body">
-              <h3 className="card-title">Ava Kim</h3>
-              <img className="arrow" src="img/icons/arrow.png" />
-              <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
-              <p className="card-text">IxD '25</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
-
-// archived data .. delete later
-const data =
-  <div className="row row-cols-1 row-cols-md-4">
-    <ProjectCard />
-    <div className="col">
-      <div className="card block">
-        <img src="img/projects/p2.jpg" className="card-img-top" alt="App Home Mockup" />
-        <div className="card-body">
-          <h3 className="card-title">App Home</h3>
-          <img className="arrow" src="img/icons/arrow.png" />
-          <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
-          <p className="card-text">Aria Donovan INFO '25</p>
-        </div>
-      </div>
-    </div>
-    <div className="col">
-      <div className="card block">
-        <img src="img/projects/p3.jpg" className="card-img-top" alt="Park Quest App Mockup" />
-        <div className="card-body">
-          <h3 className="card-title">Park Quest</h3>
-          <img className="arrow" src="img/icons/arrow.png" />
-          <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
-          <p className="card-text">Jackson Thompson IxD '25</p>
-        </div>
-      </div>
-    </div>
-    <div className="col">
-      <div className="card block">
-        <img src="img/projects/p4.jpg" className="card-img-top" alt="Shoeholic App Mockup" />
-        <div className="card-body">
-          <h3 className="card-title">Shoeholic</h3>
-          <img className="arrow" src="img/icons/arrow.png" />
-          <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
-          <p className="card-text">Mila Nguyen HCDE '24</p>
-        </div>
-      </div>
-    </div>
-  </div>
+// function GalleryPortfolios(props) {
+//   return (
+//     <section className="portfolios container">
+//       <h2 className="mt-5">Portfolios</h2>
+//       <div className="row row-cols-1 row-cols-md-4">
+//         <div className="col">
+//           <div className="card block">
+//             <a href="portfolio_example.html">
+//               <img src="img/portfolios/port1.jpeg" className="card-img-top" alt="Maya Patel's Portfolio" />
+//             </a>
+//             <div className="card-body">
+//               <h3 className="card-title">Maya Patel </h3>
+//               <img className="arrow" src="img/icons/arrow.png" />
+//               <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
+//               <p className="card-text">HCDE '26</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="col">
+//           <div className="card block">
+//             <img src="img/portfolios/port2.jpeg" className="card-img-top" alt="Noah Martinez's Portfolio" />
+//             <div className="card-body">
+//               <h3 className="card-title">Noah Martinez</h3>
+//               <img className="arrow" src="img/icons/arrow.png" />
+//               <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
+//               <p className="card-text">Info '25</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="col">
+//           <div className="card block">
+//             <img src="img/portfolios/port3.jpeg" className="card-img-top" alt="Alec Santiago's Portfolio" />
+//             <div className="card-body">
+//               <h3 className="card-title">Alec Santiago</h3>
+//               <img className="arrow" src="img/icons/arrow.png" />
+//               <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
+//               <p className="card-text">Info '24</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="col">
+//           <div className="card block">
+//             <img src="img/portfolios/port4.jpeg" className="card-img-top" alt="Ava Kim's Portfolio" />
+//             <div className="card-body">
+//               <h3 className="card-title">Ava Kim</h3>
+//               <img className="arrow" src="img/icons/arrow.png" />
+//               <img className="bookmark" src="img/icons/bookmark-white.png" alt="bookmark" />
+//               <p className="card-text">IxD '25</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
