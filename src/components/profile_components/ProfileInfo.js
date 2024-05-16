1730195178d5b09223fa11547b9de372f32148a8
Farrel Sudrajat <farevt@gmail.com>
2024-05-15 17:55:58 -0700

    Profile page layouts

--- src/components/profile_components/ProfileInfo.js
+++ src/components/profile_components/ProfileInfo.js
@@ -10,16 +10,16 @@ let profileData = {
 export default function ProfileInfo(props) {
   return (
     <article className="profile">
-      <div>
+      <div className="img-left-container align-center">
         <section>
-          <img className="profile-pic rounded-circle" src={"img/profiles/"
+          <img className="profile-pic rounded-circle mx-3" style={Object.assign({"width" : "150px"}, {"height" : "150px"})}src={"img/profiles/"
             + profileData.profilePic} alt={profileData.profilePicAlt}
           />
         </section>
-        <section className="text-left">
+        <section className="mt-1" style={{"textAlign" : "left"}}>
           <h2>{profileData.name}</h2>
           <p>{"@" + profileData.username}</p>
-          <p className="px-3 py-1 bg-purple rounded-5 text-light">{profileData.major}, {profileData.gradYear}</p>
+          <p className="my-4 px-3 py-1 bg-purple rounded-5 text-light">{profileData.major}, {profileData.gradYear}</p>
         </section>
       </div>
     </article>
