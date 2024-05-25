4877e272373a5a0243a4cc38154c47754680b872
Farrel Sudrajat <farevt@gmail.com>
2024-05-24 22:35:41 -0700

    Added project page basic components, needs styling

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -10,24 +10,69 @@ export default function ProjectPage(props) {
 
   return (
     <div className="m-5">
-      <ProjectInfo data={project}/>
       <p>
         Hiiii in progress.
       </p>
+      <div className="d-flex" style={{"gap" : "1rem"}}>
+        <ProjectInfo data={project}/>
+        <ProjectContent data={project} />
+      </div>
     </div>
   )
 }
 
-function ProjectInfo({ data }) {
-  const { metadata, authorData, intro } = data;
+function ProjectContent({ data }) {
+  const { metadata, authorData, intro, problem, solution, technicalDetails } = data;
 
   return (
-    <div>
-      <h1>{metadata.title}</h1>
-      <div className="img-left-container align-items-center">
-        <img className="profile-pic rounded-circle mx-3" style={Object.assign({"width" : "50px"}, {"height" : "50px"})} src={"/img/profiles/" + authorData.authorPicture}/>
-        <h3>{authorData.author}</h3>
+    <img src={"/img/projects/" + intro.imgSrc} style={{"width" : "30rem"}}/>
+  )
+}
+
+function ProjectInfo({ data }) {
+  const { metadata, authorData, intro, problem, solution, technicalDetails } = data;
+  const toolsUsed = technicalDetails.tools.join(", ");
+  let links = authorData.links;
+
+  if (links.length > 0) {
+    links = authorData.links.map((link, index) => {
+      let workingLink;
+
+      if(link.includes("linked.in")) {
+        workingLink = <a key={index} href={link} target="_blank">LinkedIn</a>
+      } else if (link.includes("behance.net")) {
+        workingLink = <a key={index} href={link} target="_blank">Behance</a>
+      } else {
+        workingLink = <a key={index} href={link} target="_blank">{authorData.author}'s Website</a>
+      }
+
+      return workingLink;
+    });
+  } else {
+    links = <p>None</p>
+  }
+
+  return (
+    <div className="card p-4" style={{"width" : "30rem"}}>
+      <div className="d-flex" style={{"gap" : "0.2rem"}}>
+        <p className="py-1 px-3 text-light bg-purple rounded-pill">{authorData.authorMajor}</p>
+        <p className="py-1 px-3 text-light bg-purple rounded-pill">{metadata.typeOfProj.charAt(0).toUpperCase() + metadata.typeOfProj.slice(1)}</p>
       </div>
+      <h1>{metadata.title}</h1>
+      <div className="d-flex align-items-center">
+        <img className="profile-pic rounded-circle mx-3" style={Object.assign({"width" : "50px"}, {"height" : "50px"})} src={"/img/profiles/" + authorData.authorPicture}/>
+        <h5 className="heading-body-typeface">{authorData.author}</h5>
+      </div>
+      <p>{intro.description}</p>
+      <p><b>Problem</b></p>
+      <p>{problem.description}</p>
+      <p><b>Solution</b></p>
+      <p>{solution.description}</p>
+      <hr />
+      <h6><b>Tools</b></h6>
+      <p>{toolsUsed}</p>
+      <h6><b>Links</b></h6>
+      {links}
     </div>
   )
 
