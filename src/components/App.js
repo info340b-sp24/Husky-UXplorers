40514aff5ca4683befac60f84c5a3e8a1246e7ac
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-05-28 14:16:29 -0700

    starting to control form

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import PROJECT_DATA from '../data/projects.json';
 import Home from './Home.js';
@@ -10,18 +10,27 @@ import Guide from './Guide.js';
 import CreateProject from './CreateProject.js';
 import PageNotFound from './PageNotFound.js';
 
-export default function App(props) {
+export default function App() {
+  const [projectData, setProjectData] = useState(PROJECT_DATA);
+
+  function uploadProject(newProject) {
+    let newProjectData = [...projectData, newProject];
+    setProjectData(newProjectData);
+  }
+
   return (
     <Routes>
       <Route path = "" element = {<Home />} />
       <Route path = "index" element = {<Home />} />
       <Route path = "gallery" element = {<Gallery />} >
         <Route path = ":projectName" element = {<ProjectPage />} />
-        <Route index element = {<GalleryMain projectData={PROJECT_DATA} />}/>
+        <Route index element = {<GalleryMain projectData={projectData}/>} />
       </Route>
       <Route path = "profile" element = {<Profile />} />
       <Route path = "guide" element = {<Guide />} />
-      <Route path = "create-project" element = {<CreateProject/>} />
+      <Route path = "create-project" element =
+        {<CreateProject uploadProject={uploadProject} />}
+      />
       <Route path = "*" element = {<PageNotFound />} />
     </Routes>
   );
--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -3,46 +3,54 @@ import NavBar from './NavBar';
 import Footer from './Footer'
 
 export default function CreateProject (props) {
+  const { uploadProject } = props;
+
   return (
     <div>
       <NavBar />
-      <MainCreateProject />
+      <MainCreateProject uploadProject={uploadProject}/>
       <Footer />
     </div>
   );
 }
 
 function MainCreateProject (props) {
+  const { uploadProject } = props;
+
+  const handleSubmit = function (newProject) {
+    uploadProject(newProject);
+  }
+
   return (
     <main className="container-fluid">
       <h1>New Project</h1>
       <p className="small-text">Enter in information to post a new project</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <section>
           <h2>Project Introduction</h2>
           <div className="form-floating mb-3">
             <input type="text" className="form-control" id="project-name" placeholder="ex: Cooking Master App" />
-            <label for="project-name">Project Name</label>
+            <label htmlFor="project-name">Project Name</label>
           </div>
 
           <div className="form-floating mb-3">
             <input type="text" className="form-control" id="project-authors" placeholder="ex: Joe and Laura" />
-            <label for="project-authors">Author Name(s)</label>
+            <label htmlFor="project-authors">Author Name(s)</label>
           </div>
 
           <div>
-            <label for="project-type-option1" className="form-label mb-3">Type of Project: </label>
+            <label htmlFor="project-type-option1" className="form-label mb-3">Type of Project: </label>
             <div className="form-check form-check-inline">
               <input className="form-check-input" type="radio" id="project-type-option1" value="option1" />
-              <label className="form-check-label" for="project-type-option1">For School</label>
+              <label className="form-check-label" htmlFor="project-type-option1">For School</label>
             </div>
             <div className="form-check form-check-inline">
               <input className="form-check-input" type="radio" id="project-type-option2" value="option2" />
-              <label className="form-check-label" for="project-type-option2">For a Client</label>
+              <label className="form-check-label" htmlFor="project-type-option2">For a Client</label>
             </div>
             <div className="form-check form-check-inline">
               <input className="form-check-input" type="radio" id="project-type-option3" value="option3" />
-              <label className="form-check-label" for="project-type-option3">For Fun</label>
+              <label className="form-check-label" htmlFor="project-type-option3">For Fun</label>
             </div>
           </div>
 
@@ -52,11 +60,11 @@ function MainCreateProject (props) {
               id="project-description"
               style={{height: "100px"}}>
             </textarea>
-            <label for="project-description">Give an introduction to the project</label>
+            <label htmlFor="project-description">Give an introduction to the project</label>
           </div>
 
           <div className="mb-3">
-            <label for="project-images" className="form-label">Add images</label>
+            <label htmlFor="project-images" className="form-label">Add images</label>
             <input className="form-control" type="file" id="project-images" multiple />
           </div>
         </section>
@@ -69,10 +77,10 @@ function MainCreateProject (props) {
               id="project-problem"
               style={{height: "100px"}}>
             </textarea>
-            <label for="project-problem">Give a description of the problem</label>
+            <label htmlFor="project-problem">Give a description of the problem</label>
           </div>
           <div className="mb-3">
-            <label for="project-problem-images" className="form-label">Add Images</label>
+            <label htmlFor="project-problem-images" className="form-label">Add Images</label>
             <input className="form-control" type="file" id="project-problem-images" multiple />
           </div>
         </section>
@@ -85,10 +93,10 @@ function MainCreateProject (props) {
               id="project-solution"
               style={{height: "100px"}}>
             </textarea>
-            <label for="project-solution">Give a description of the solution</label>
+            <label htmlFor="project-solution">Give a description of the solution</label>
           </div>
           <div className="mb-3">
-            <label for="project-solution-images" className="form-label">Add Images</label>
+            <label htmlFor="project-solution-images" className="form-label">Add Images</label>
             <input className="form-control" type="file" id="project-solution-images" multiple />
           </div>
         </section>
@@ -97,33 +105,35 @@ function MainCreateProject (props) {
           <h2>Technical Details</h2>
           <div className="form-floating mb-3">
             <input type="text" className="form-control" id="project-date" placeholder="ex: March 2021" />
-            <label for="project-date">Month and year of project</label>
+            <label htmlFor="project-date">Month and year of project</label>
           </div>
           <div className="mb-3">
             <h3 className="fs-5">Tools Used</h3>
 
             <input type="checkbox" className="btn-check" id="btn-check-1" autocomplete="off" />
-            <label className="btn" for="btn-check-1">Figma</label>
+            <label className="btn" htmlFor="btn-check-1">Figma</label>
 
             <input type="checkbox" className="btn-check" id="btn-check-2" autocomplete="off" />
-            <label className="btn" for="btn-check-2">Adobe XD</label>
+            <label className="btn" htmlFor="btn-check-2">Adobe XD</label>
 
             <input type="checkbox" className="btn-check" id="btn-check-3" autocomplete="off" />
-            <label className="btn" for="btn-check-3">Framer</label>
+            <label className="btn" htmlFor="btn-check-3">Framer</label>
 
             <input type="checkbox" className="btn-check" id="btn-check-4" autocomplete="off" />
-            <label className="btn" for="btn-check-4">WebFlow</label>
+            <label className="btn" htmlFor="btn-check-4">WebFlow</label>
 
             <input type="checkbox" className="btn-check" id="btn-check-5" autocomplete="off" />
-            <label className="btn" for="btn-check-5">Sketch</label>
+            <label className="btn" htmlFor="btn-check-5">Sketch</label>
           </div>
         </section>
 
-        <a className="btn rounded px-4 py-3 bg-dark text-white"
+        <button
+          className="rounded px-4 py-3 bg-dark text-white"
           href="profile-finished.html"
-          role="button">
+          type="submit"
+        >
           Sumbit Project
-        </a>
+        </button>
       </form>
     </main>
   );
