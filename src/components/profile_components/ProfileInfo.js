4c1e7fd63108ca923591bb2659cb587e890839dd
Farrel Sudrajat <farevt@gmail.com>
2024-06-03 19:52:48 -0700

    made profile info a card

--- src/components/profile_components/ProfileInfo.js
+++ src/components/profile_components/ProfileInfo.js
@@ -11,7 +11,6 @@ const defaultProfileData = {
 };
 
 export default function ProfileInfo(props) {
-  const [isEditing, setIsEditing] = useState(false);
   const [profileData, setProfileData] = useState(defaultProfileData);
 
   const currentUser = props.currentUser || profileData;
@@ -22,24 +21,47 @@ export default function ProfileInfo(props) {
   };
 
   return (
+    // <div className="profile">
+    //   <div className="img-left-container">
+    //     <section>
+    //       <img
+    //         className="profile-pic rounded-circle mx-3"
+    //         style={{ width: "150px", height: "150px" }}
+    //         src={currentUser.userImg}
+    //         alt={currentUser.username + "'s profile icon"}
+    //       />
+    //     </section>
+    //     <section className="mt-1" style={{ textAlign: "left" }}>
+    //         <div>
+    //           <h1>{currentUser.username}</h1>
+    //           <p>{"@" + currentUser.usertag}</p>
+    //           <p className="badge px-3 py-2 bg-purple rounded-5 text-light">{currentUser.major + ", " + currentUser.graduatingYear}</p>
+    //           <button className="btn" onClick={handleEditClick}>Edit Profile</button>
+    //         </div>
+    //     </section>
+    //   </div>
+    // </div>
+
     <div className="profile">
-      <div className="img-left-container">
-        <section>
-          <img
-            className="profile-pic rounded-circle mx-3"
-            style={{ width: "150px", height: "150px" }}
-            src={currentUser.userImg}
-            alt={currentUser.username + "'s profile icon"}
-          />
-        </section>
-        <section className="mt-1" style={{ textAlign: "left" }}>
-            <div>
-              <h1>{currentUser.username}</h1>
-              <p>{"@" + currentUser.usertag}</p>
-              <p className="badge px-3 py-2 bg-purple rounded-5 text-light">{currentUser.major + ", " + currentUser.graduatingYear}</p>
-              <button className="btn" onClick={handleEditClick}>Edit Profile</button>
-            </div>
-        </section>
+      <div className="card mb-3">
+        <div className="row g-0">
+          <section className="col-md-4">
+            <img
+              className="profile-pic rounded-circle mx-3 bg-dark"
+              style={{ width: "150px", height: "150px" }}
+              src={currentUser.userImg}
+              alt={currentUser.username + "'s profile icon"}
+            />
+          </section>
+          <section className="col-md-8" style={{ textAlign: "left" }}>
+              <div className="card-body">
+                <h1 className="card-title">{currentUser.username}</h1>
+                <p className="card-text">{"@" + currentUser.usertag}</p>
+                <p className="badge px-3 py-2 bg-purple rounded-5 text-light">{currentUser.major + ", " + currentUser.graduatingYear}</p>
+                <button className="btn" onClick={handleEditClick}>Edit Profile</button>
+              </div>
+          </section>
+        </div>
       </div>
     </div>
   );
